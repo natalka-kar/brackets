e4b7ee845679801042dfe30f25653ba8a32c7778
natalka-kar <nataryades@gmail.com>
2022-07-26 19:39:06 +0300

    init: start and finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let finishArr = [];
+  let newStr = str.split('');
+  for (let n = 0; n < bracketsConfig.length; n++) {
+    for (let i = 0; i < newStr.length; i++) {
+      if ((newStr[i - 1] === bracketsConfig[n][0] && newStr[i] === bracketsConfig[n][1])) {
+        newStr.splice(i - 1, 2);
+        i = 0;
+        n = 0;
+      }
+    }
+  }
+  if (newStr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
